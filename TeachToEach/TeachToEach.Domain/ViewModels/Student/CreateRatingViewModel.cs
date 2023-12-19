58d54f3ed87fd6f07e006e37283dfff459650072
GeorgeD615 <st095277@student.spbu.ru>
2023-12-19 23:36:20 +0300

    CreateRating_StudentService

---
+++ TeachToEach/TeachToEach.Domain/ViewModels/Student/CreateRatingViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeachToEach.Domain.ViewModels.Student
+{
+    public class CreateRatingViewModel
+    {
+        public int rating_value { get; set; }
+        public string review { get; set; }
+        public int relation_id { get; set; }
+    }
+}
---
+++ TeachToEach/TeachToEach.Domain/ViewModels/Teacher/RatingViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeachToEach.Domain.ViewModels.User;
+
+namespace TeachToEach.Domain.ViewModels.Teacher
+{
+    public class RatingViewModel
+    {
+        public int rating_value { get; set; }
+        public string review { get; set; }
+        public UserViewModel student { get; set; }
+
+    }
+}
