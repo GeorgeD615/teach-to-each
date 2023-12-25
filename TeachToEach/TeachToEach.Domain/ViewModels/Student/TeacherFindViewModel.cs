06aa6f58117f3578a83ae410a3bd90101f501e26
GeorgeD615 <st095277@student.spbu.ru>
2023-12-25 17:52:42 +0300

    add jquery validation check

--- TeachToEach/TeachToEach.Domain/ViewModels/Student/TeacherFindViewModel.cs
+++ TeachToEach/TeachToEach.Domain/ViewModels/Student/TeacherFindViewModel.cs
@@ -9,8 +9,12 @@ namespace TeachToEach.Domain.ViewModels.Student
 {
     public class TeacherFindViewModel
     {
-        
+        [MaxLength(20, ErrorMessage = "Имя должно иметь длину меньше 20 символов")]
+        [MinLength(2, ErrorMessage = "Имя должно иметь длину больше 2 символов")]
         public string? first_name { get; set; }
+
+        [MaxLength(20, ErrorMessage = "Фамилия должна иметь длину меньше 20 символов")]
+        [MinLength(2, ErrorMessage = "Фамилия должна иметь длину больше 2 символов")]
         public string? last_name { get; set; }
 
 
